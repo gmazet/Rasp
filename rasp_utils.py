cbad00cba173df0ef802cf040b91879073655b52
Gilles Mazet-Roux <gilles.mazet-roux@cea.fr>
2019-07-17 09:55:38 +0200

    Simplificatin and correction for handling RESIF data

--- rasp_utils.py
+++ rasp_utils.py
@@ -4,22 +4,27 @@ my_stations=['R9F1B','RE4C0','R57C7','R0B29','RA3B7']
 my_stations=['RA007']
 my_stations=['R573E','RD51C','RA007','RB707']
 my_stations=['R9F1B','RDF31','R8F35','RE4C0','R57C7','R0B29','RA3B7']
+my_stations=['RA3B7','R57C7','R4203'] # PA, EC, Maconnais
+my_stations=['R9F1B','R8F35','RE4C0'] # GMR, BG, CM
+my_stations=['R9F1B','R8F35','RBFD5','RDF31'] # GMR, BG, NB, JPS
+my_stations=['R9F1B','RBFD5','RE4C0'] # GMR, NB, CM
+my_stations=['R9F1B','R8F35','RBFD5','RE4C0'] # GMR, BG, NB, CM
+my_stations=['R9F1B','RBFD5'] # GMR, NB
 
-NODATA=('R6E96','R052F','R1FBA','RB511','SFD6B', 'RA70D','R9DA3','R5661','RE7F9','R9DBD','R3F3B','ILLF')
+NODATA=('R6E96','R052F','R1FBA','RB511','SFD6B', 'RA70D','R9DA3','R5661','RE7F9','R9DBD','R3F3B','ILLF','RA14B','R440A')
 
-HOST1="gmazet.freeboxos.fr"
-HOST2="rtserve.resif.fr"
-HOST3="rtserve.iris.washington.edu"
-HOST5="geofon.gfz-potsdam.de"
-HOST6="geosrt1.ipgp.fr"
-HOST6="rtserver.ipgp.fr"
-HOST6="eida.ipgp.fr"
-HOST7="caps.raspberryshakedata.com"
-HOST8="fdsnws.raspberryshakedata.com"
+HOST1="rtserve.resif.fr"
+HOST2="rtserve.iris.washington.edu"
+HOST3="geofon.gfz-potsdam.de"
+HOST4="geosrt1.ipgp.fr"
+HOST5="rtserver.ipgp.fr"
+HOST6="webservices.ingv.it"
+HOST7="fdsnws.raspberryshakedata.com"
 
 BEFORE=10
 myradius=10.0
 
+"""
 #-------------------------------------------------------------------
 def counts2Amp (c): #Only for Raspberryshake data (not reliable)
     # Av=470*c ( 470 counts ~ 1 um/s according to sensor sensentivity provided by sensor technical specs)
@@ -27,18 +32,19 @@ def counts2Amp (c): #Only for Raspberryshake data (not reliable)
     Av=float(c)/470
     Ad=pow(10,(Av/(2*pi)))
     return Av,Ad
+"""
     
 
 # Build station book
 # ------------------
-def build_station_list(ev,provider="resif"):
+def build_station_list(ev,provider="rasp"):
     i=0
     ALLSTATIONS=[]
     if (provider=="rasp"):
         NET="AM"
         LOCCODE="00"
         CHANNEL="SHZ"
-        HOST=HOST8
+        HOST=HOST7
         RASP_STATIONBOOK="%s/stations_raspberryshake.txt" % DATADIR
         print "RASP_STATIONBOOK=%s" % RASP_STATIONBOOK
         fsta=open(RASP_STATIONBOOK,'r')
@@ -56,8 +62,8 @@ def build_station_list(ev,provider="resif"):
         #client=RoutingClient("iris-federator")
         #client=RoutingClient("eida-routing")
         client=Client(provider)
-        inv = client.get_stations( channel="HHZ", starttime=ev.OTutc, endtime=ev.OTutc, latitude=ev.lat, level="channel", longitude=ev.lon, maxradius=myradius, includerestricted=False)
-        #print inv
+        inv = client.get_stations( channel="HHZ", network="FR", starttime=ev.OTutc, endtime=ev.OTutc, latitude=ev.lat, level="channel", longitude=ev.lon, maxradius=myradius, includerestricted=False)
+        print "Found %d stations" % len(inv)
 
         ALLCHAN=inv.get_contents()['channels']
         for c in ALLCHAN:
