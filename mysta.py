ad0ce1864d8a8187f9a52c63aa4976e17e634777
Gilles Mazet-Roux <gilles.mazet-roux@cea.fr>
2019-11-29 17:07:49 +0100

    modif

--- mysta.py
+++ mysta.py
@@ -1,5 +1,8 @@
-my_stations=['R9F1B']
 my_stations=['R9F1B','RBFD5','RDF31']
+my_stations=['R9F1B','RE4C0']
+#my_stations=['R9F1B','R8F35','RBFD5','RC7DF']
+my_stations=['R9F1B','R8F35','RBFD5']
+my_stations=['R9F1B','RFD43']
 
 NODATA=('R6E96','R052F','R1FBA','RB511','SFD6B', 'RA70D','R9DA3','R5661','RE7F9','R9DBD','R3F3B','ILLF','RA14B','R440A')
 
